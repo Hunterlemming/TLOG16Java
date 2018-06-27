094b384eae5f232681f3b7a501d90ad93f5cdb5b
Hunterlemming <kklemming@gmail.com>
2018-06-27 13:34:37 +0200

    --- SUMMARY ---
    WorkMonth Object methods implemented.

--- src/tlog16java/WorkMonth.java
+++ src/tlog16java/WorkMonth.java
@@ -4,10 +4,65 @@ import java.util.List;
 import java.time.YearMonth;
 
 public class WorkMonth {
+            
+//---- Variables ----
     
     private List<WorkDay> days;
     private YearMonth date;
     private long sumPerMonth;
     private long requiredMinPerMonth;
+        
+//---- Constructors, Getters ----
+    
+    public WorkMonth(int _year, int _month){
+        date=YearMonth.of(_year,_month);
+    }
+    
+    public List<WorkDay> getDays() {
+        return days;
+    }
+    
+    public YearMonth getDate() {
+        return date;
+    }
+    
+    public long getSumPerMonth() {
+        return sumPerMonth;
+    }
+    
+    public long getRequiredMinPerMonth() {
+        return requiredMinPerMonth;
+    }
+        
+//---- User Methods ----       
+    
+    public long getExtraMinPerMonth(){
+        return sumPerMonth-requiredMinPerMonth;
+    }
+    
+    public boolean isNewDate(WorkDay wd){
+        boolean isNew=true;
+        for (int i=0; i<days.size(); i++){
+            if (wd.equals(days.get(i))){
+                isNew=false;
+            }
+        }
+        return isNew;
+    }
+    
+    public boolean isSameMonth(WorkDay wd){
+        if(wd.getActualDay().getMonth()==date.getMonth()){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public void addWorkDay(WorkDay wd, boolean isWeekendEnabled){
+        if ( isNewDate(wd)==true && isSameMonth(wd)==true )
+            if ( isWeekendEnabled==true || ( wd.isWeekDay() && isWeekendEnabled==false ) ){
+                days.add(wd);
+            }
+    }
     
 }
