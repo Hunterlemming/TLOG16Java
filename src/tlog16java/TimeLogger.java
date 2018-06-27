1a2f74ce6129bc22265399ec45f711ce36d02744
Hunterlemming <kklemming@gmail.com>
2018-06-27 13:48:19 +0200

    --- SUMMARY ---
    WorkMonth addWorkDay() overloaded.
    TimeLogger object methods implemented.

--- src/tlog16java/TimeLogger.java
+++ src/tlog16java/TimeLogger.java
@@ -3,7 +3,32 @@ package tlog16java;
 import java.util.List;
 
 public class TimeLogger {
+            
+//---- Variables ----
     
     private List<WorkMonth> months;
+        
+//---- User Methods ----       
     
+    public List<WorkMonth> getMonths() {
+        return months;
+    }
+        
+//---- User Methods ----       
+    
+    public boolean isNewMonth(WorkMonth wm){
+        boolean isNew=true;
+        for (int i=0; i<months.size(); i++){
+            if (wm.equals(months.get(i))){
+                isNew=false;
+            }
+        }
+        return isNew;
+    }
+    
+    public void addMonth(WorkMonth wm){
+        if (isNewMonth(wm)){
+            months.add(wm);
+        }
+    }
 }
