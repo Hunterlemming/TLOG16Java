5cacba0b008e09f006c73613fa641f1944b51fc1
Hunterlemming <kklemming@gmail.com>
2018-07-02 09:48:27 +0200

    --- SUMMARY ---
    Minor but vital changes to TLOG16Java and TimeLoggerUI
    
    --- TimeLoggerUI ---
    
    listDaysForMonth() and listTasksForDay() methods created

--- src/tlog16java/TimeLoggerUI.java
+++ src/tlog16java/TimeLoggerUI.java
@@ -1,13 +1,20 @@
 package tlog16java;
 
+import java.util.Scanner;
+
 public class TimeLoggerUI {
     
-    public void showMenu(){
+    private Scanner userInput = new Scanner(System.in);
+    private TimeLogger workLog;
+    
+    public void showMenu(TimeLogger _workLog){
+        workLog=_workLog;
         System.out.println("-----------------------------------------------");
         System.out.println("Please choose an operation from the list below!");
         System.out.println("0. Exit");
         System.out.println("1. List months");
-        System.out.println("2. List days");
+        System.out.println("2. List days for a specific month");
+        System.out.println("3. List tasks for a specific day");
         System.out.println("-----------------------------------------------");
     }
     
@@ -20,7 +27,10 @@ public class TimeLoggerUI {
                 listMonths();
                 break;
             case 2:
-                
+                listDaysForMonth();
+                break;
+            case 3:
+                listTasksForDay();
                 break;
             default:
                 System.out.println("Number \"" + procedure + "\" does not refer to a procedure!");
@@ -30,9 +40,33 @@ public class TimeLoggerUI {
     }
     
         private void listMonths(){
-            TimeLogger timelog = new TimeLogger();
-            for (int i=0; i<timelog.getMonths().size(); i++){
-                System.out.println( (i+1) + ". " + timelog.getMonths().get(i).getDate().getYear() + "-" + timelog.getMonths().get(i).getDate().getMonth() );
+            for (int i=0; i<workLog.getMonths().size(); i++){
+                System.out.println( (i+1) + ". " + workLog.getMonths().get(i).getDate().getYear() + "-" + workLog.getMonths().get(i).getDate().getMonth() );
+            }
+        }
+        
+        private WorkMonth listDaysForMonth(){
+            listMonths();
+            System.out.println("Choose a month to list workdays from!");
+            int option=userInput.nextInt()-1;
+            
+            WorkMonth chosenMonth = workLog.getMonths().get(option);
+            for(int i=0; i<chosenMonth.getDays().size(); i++){
+                System.out.println((i+1) + " " + chosenMonth.getDays().get(i).getActualDay());
+            }
+            
+            return chosenMonth;
+        }
+        
+        private void listTasksForDay(){
+            WorkMonth chosenMonth = listDaysForMonth();
+            System.out.println("Choose a day to list tasks from!");
+            int option=userInput.nextInt()-1;
+            
+            WorkDay chosenDay = chosenMonth.getDays().get(option);
+            for (int i=0; i<chosenDay.getTasks().size(); i++){
+                System.out.println("TaskId: " + chosenDay.getTasks().get(i).getTaskId() + ", Comment: " + chosenDay.getTasks().get(i).getComment());
+                System.out.println("StartTime: " + chosenDay.getTasks().get(i).getStartTime() + ", EndTime: " + chosenDay.getTasks().get(i).getEndTime() +"\n");
             }
         }
     
