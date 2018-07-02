a5f171e4d126ca1a877c29d7bb12b657e561d974
Hunterlemming <kklemming@gmail.com>
2018-07-02 09:48:43 +0200

    --- SUMMARY ---
    Minor but vital changes to TLOG16Java and TimeLoggerUI
    
    --- TimeLoggerUI ---
    
    listDaysForMonth() and listTasksForDay() methods created

--- src/tlog16java/TLOG16Java.java
+++ src/tlog16java/TLOG16Java.java
@@ -9,14 +9,16 @@ public class TLOG16Java {
         application.start();
     }
     
+    private Scanner userInput = new Scanner(System.in);
+    private TimeLogger workLog = new TimeLogger();
+    
     public void start() {
         System.out.println("Precognox TimeLogger Project");
         TimeLoggerUI UI = new TimeLoggerUI();
-        Scanner userInput = new Scanner(System.in);
         
         boolean next = true;
         while (next==true){
-            UI.showMenu();
+            UI.showMenu(workLog);
             int procedure = userInput.nextInt();
             next = UI.execute(procedure);
         }
